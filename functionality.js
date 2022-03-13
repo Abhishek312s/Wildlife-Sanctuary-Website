e9ff2163c5c9941afc3dc340dddaf81433667729
Abhishek <sachdevaabu30@gmail.com>
2022-03-14 00:21:51 +0530

    floating button to top added

---
+++ functionality.js
@@ -0,0 +1,20 @@
+//Get the button:
+floatButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    floatButton.style.display = "block";
+    floatButton.style.transform = "translateX(0)";
+  } else {
+    floatButton.style.transform = "translateX(500%)";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
