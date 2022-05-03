4c042eb61b3183783db4616e081c94e32cc75b13
Abhishek <sachdevaabu30@gmail.com>
2022-05-03 20:20:55 +0530

    gallery page and matters section

--- functionality.js
+++ functionality.js
@@ -2,7 +2,7 @@
 floatButton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -18,3 +18,52 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+let Gbtn = document.getElementById("Gbtn");
+let Gwrapper = document.getElementById("Gwrapper");
+
+function morePhotos() {
+
+  Gwrapper.innerHTML += `<div>
+  <img src="./imgs/gimg21.jpg"
+      alt="" />
+</div>
+<div class="wide">
+  <img src="./imgs/gimg22.jpg"
+      alt="" />
+</div>
+<div>
+  <img src="./imgs/gimg23.jpg"
+  alt="" />
+</div>
+<div>
+  <img src="./imgs/gimg24.jpg"
+  alt="" />
+</div>
+<div class="tall">
+  <img src="./imgs/gimg25.jpg"
+  alt="" />
+</div>
+<div class="wide">
+  <img src="./imgs/gimg26.jpg"
+  alt="" />
+</div>
+<div>
+  <img src="./imgs/gimg5.jpg"
+  alt="" />
+</div>
+<div class="wide">
+  <img src="./imgs/gimg18.jpg"
+  alt="" />
+</div>
+<div class="tall">
+  <img src="./imgs/gimg29.jpg"
+  alt="" />
+</div>`
+}
+
+Gbtn.addEventListener('click', () => {
+
+  morePhotos();
+});
